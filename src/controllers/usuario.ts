95b823bafab2a4f9b5328c844c358f4c30b2785b
Eliton Camargo <camargoliveira@gmail.com>
2024-10-07 11:37:09 -0300

    Criando função de cadastrar Usuario

--- src/controllers/usuario.ts
+++ src/controllers/usuario.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import prisma from '../config/database';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
-export const getUsuarios = async (req: Request, res: Response) => {
+export const consultar = async (req: Request, res: Response) => {
   try {
     const users = await prisma.usuario.findMany(); // Buscar muitos
     res.json(users).status(200);
@@ -11,7 +13,7 @@ export const getUsuarios = async (req: Request, res: Response) => {
   }
 };
 
-export const buscarPorId = async (req: Request, res: Response)=>{
+export const consultarPorId = async (req: Request, res: Response)=>{
   try{
     const _id = parseInt(req.params.id);
     const usuario = await prisma.usuario.findUnique(
@@ -22,5 +24,41 @@ export const buscarPorId = async (req: Request, res: Response)=>{
   catch(error){
     console.error(error);
     res.status(500).json({ error: 'Erro ao buscar usuários' });
+  }
+}
+
+export const cadastrar = async (req: Request, res: Response)=>{
+  try{
+    const {nome, email, senha, data_nasc} = req.body;
+      // Validação básica
+      if (!nome || !email || !senha || !data_nasc) {
+        return res.status(400).json({
+          result: false,
+          data: null,
+          info: "Nome, email e senha são obrigatórios"
+        });
+      }
+
+      // Verificar se o email já está cadastrado
+      const usuarioExistente = await prisma.usuario.findUnique({
+        where: { email }
+      });
+
+      if (usuarioExistente) {
+        return res.status(400).json({
+          result: false,
+          data: null,
+          info: "Email já cadastrado"
+        });
+      }
+
+      // Criptografar a senha antes de salvar
+      const salt = await bcrypt.genSalt(5);
+      const senhaCriptografada = await bcrypt.hash(senha, salt);
+
+  }
+  catch(error){
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao cadastrar usuários' });
   }
 }
