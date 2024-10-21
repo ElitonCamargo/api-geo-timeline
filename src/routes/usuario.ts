222f53aff580093c3dab2d246b8afdb244349b80
ELiton Camargo <camargoliveira@gmail.com>
2024-10-20 21:55:55 -0300

    Finalizando funcionalidades da controller Usuário

--- src/routes/usuario.ts
+++ src/routes/usuario.ts
@@ -3,7 +3,12 @@ import * as usuario from '../controllers/usuario';
 
 const rota = Router();
 
-rota.get('/usuario', usuario.getUsuarios);
-rota.get('/usuario/:id', usuario.buscarPorId);
+rota.get('/usuario', usuario.consultar);
+rota.get('/usuario/:id', usuario.consultarPorId);
+rota.get('/usuario/email/:email', usuario.consultarPorEmail);
+rota.post('/usuario', usuario.cadastrar);
+rota.post('/usuario/login', usuario.login);
+rota.delete('/usuario/:id', usuario.deletar);
+rota.put('/usuario/:id', usuario.alterar);
 
 export default rota;
