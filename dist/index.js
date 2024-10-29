d0a3ea0add1478edfc8b3742742836babd002ff9
ELiton Camargo <camargoliveira@gmail.com>
2024-10-29 09:22:30 -0300

    Config deploy

--- dist/index.js
+++ dist/index.js
@@ -19,5 +19,5 @@ app.get('/', (req, res) => {
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT} (http://localhost:${PORT})`);
+    console.log('Servidor rodando.', `Porta ${PORT} (http://localhost:${PORT})`);
 });
