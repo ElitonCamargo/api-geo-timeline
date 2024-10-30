aa16e9c60bcb7a442342f8d398c8d95f2a12597e
ELiton Camargo <camargoliveira@gmail.com>
2024-10-29 22:34:27 -0300

    Vercel config 07

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log('Servidor rodando.',`Porta ${PORT} (http://localhost:${PORT})`);
 });
