83fb3d6f3856f2ecd36489a6e764c911c535dd57
ELiton Camargo <camargoliveira@gmail.com>
2024-11-04 13:59:05 -0300

    Config .env Postam

--- dist/controllers/usuario.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-export declare const cadastrar: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const login: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const consultar: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const consultarPorEmail: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const usuarioLogado: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const consultarPorId: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const deletar: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const alterar: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
